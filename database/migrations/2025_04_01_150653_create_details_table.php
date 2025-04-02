d660f16f03050ac18a5b5d4fa80c0a6d031fe6c6
Douglas Leandro <douglas.gtads@gmail.com>
2025-04-02 13:27:34 -0300

    :wrench: build: Changing multi-primary key to "multi-unique" columns
    SQLite needs it to execute in testing environment.

--- database/migrations/2025_04_01_150653_create_details_table.php
+++ database/migrations/2025_04_01_150653_create_details_table.php
@@ -12,12 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('details', function (Blueprint $table) {
-            $table->bigInteger('id', true, true);
-            $table->bigInteger(column: 'characteristics_id', unsigned: TRUE)->index('fk_details_characteristics1_idx');
+            $table->unsignedBigInteger('id', TRUE);
+            $table->unsignedBigInteger('characteristics_id')->nullable(FALSE)->index('fk_details_characteristics1_idx');
+            $table->unique(['id', 'characteristics_id']);
             $table->string('type');
             $table->timestamps();
-
-            $table->primary(['id', 'characteristics_id']);
         });
         Schema::table('details', function (Blueprint $table) {
             $table->foreign(['characteristics_id'], 'fk_Details_characteristics1')->references(['id'])->on('characteristics')->onUpdate('no action')->onDelete('no action');
