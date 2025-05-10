692c59f2f3039ab5c2a4e42f11d3414cbd590aaa
Douglas Leandro <douglas.gtads@gmail.com>
2025-05-10 11:54:42 -0300

    🔧 refactor: Simplifying method calls
    Include also:
    - Remotion of magic numbers by response's status code

--- tests/Feature/WebRoutesTest.php
+++ tests/Feature/WebRoutesTest.php
@@ -1,13 +1,12 @@
 <?php
 
+use Illuminate\Http\Response;
 
 describe('Web application routes', function () {
     it('returns 404 in route "/"', function () {
-        $response = $this->get('/');
-        $response->assertStatus(404);
+        $this->get('/')->assertStatus(Response::HTTP_NOT_FOUND);
     });
     it('returns 404 in whatever route', function () {
-        $response = $this->get('/any-route');
-        $response->assertStatus(404);
+        $this->get('/any-route')->assertStatus(Response::HTTP_NOT_FOUND);
     });
 });
