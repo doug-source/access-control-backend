1c1e41c2eb4b02e55e7be561fd05a15587af5938
Douglas Leandro <douglas.gtads@gmail.com>
2025-05-10 11:52:52 -0300

    ✨ feat: Creating new Socialite structure folder testing

---
+++ tests/Feature/Socialite/ApiTest.php
@@ -0,0 +1,83 @@
+<?php
+
+use App\Models\Provider;
+use Illuminate\Support\Str;
+use Illuminate\Support\Uri;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+describe('Socialite from api routes', function () {
+    describe('fails because', function () {
+        it('tries token release but user is not authenticated', function () {
+            $response = $this->postJson(route('release.token'));
+            $response->assertStatus(401);
+        });
+    });
+    describe('receives successful because', function () {
+        it('tries token release with user authenticated and email verified', function () {
+            $userModel = buildSocialite(
+                password: NULL,
+                email: 'someone@test.com',
+            );
+            Provider::factory(count: 1)->create([
+                'user_id' => $userModel->id
+            ]);
+            $this->get(route('oauth.redirect', ['provider' => 'google', 'type' => 'login']));
+            $responseRedirect = $this->get(route('oauth.callback', 'google'));
+
+            $uri = Uri::of($responseRedirect->getTargetUrl());
+            $token = Str::of($uri->query()->get('provided'))->after('|')->toString();
+
+            $this->postJson(route('release.token'), [], [
+                'Authorization' => "Bearer {$token}"
+            ])
+                ->assertOk()
+                ->assertJson(function (AssertableJson $json) use ($userModel, $token) {
+                    $json
+                        ->has('user', function (AssertableJson $json) use ($userModel, $token) {
+                            $json
+                                ->where('id', $userModel->id)
+                                ->where('name', $userModel->name)
+                                ->has('token')
+                                ->whereNot('token', $token)
+                                ->where('email', $userModel->email)
+                                ->where('emailVerified', TRUE);
+                        });
+                });
+        });
+        it('tries token release with user authenticated and email not verified', function () {
+            $userModel = buildSocialite(
+                password: NULL,
+                email: 'someone@test.com',
+                emailVerified: FALSE
+            );
+            Provider::factory(count: 1)->create([
+                'user_id' => $userModel->id
+            ]);
+            $this->get(route('oauth.redirect', ['provider' => 'google', 'type' => 'login']));
+            $responseRedirect = $this->get(route('oauth.callback', 'google'));
+
+            $uri = Uri::of($responseRedirect->getTargetUrl());
+            $token = Str::of($uri->query()->get('provided'))->after('|')->toString();
+
+            $this->postJson(route('release.token'), [], [
+                'Authorization' => "Bearer {$token}"
+            ])
+                ->assertOk()
+                ->assertJson(function (AssertableJson $json) use ($userModel, $token) {
+                    $json
+                        ->has('user', function (AssertableJson $json) use ($userModel, $token) {
+                            $json
+                                ->where('id', $userModel->id)
+                                ->where('name', $userModel->name)
+                                ->has('token')
+                                ->whereNot('token', $token)
+                                ->where('email', $userModel->email)
+                                ->where('emailVerified', FALSE);
+                        });
+                });
+        });
+    });
+});
---
+++ tests/Feature/Socialite/WebTest.php
@@ -0,0 +1,127 @@
+<?php
+
+use App\Library\Builders\Phrase;
+use App\Library\Builders\UrlExternal;
+use App\Library\Enums\PhraseKey;
+use App\Models\Provider;
+use GuzzleHttp\{
+    Psr7\Request,
+    Psr7\Response,
+    Exception\ClientException
+};
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response as HttpResponse;
+use Laravel\Socialite\{
+    Facades\Socialite,
+    Two\GoogleProvider,
+};
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+function makeClientException(string $uri)
+{
+    $request = new Request('POST', $uri);
+    $response = new Response(404);
+    return new ClientException('Client error', $request, $response);
+}
+function configSocialiteGoogleProvider(string $redirectUrl)
+{
+    $driver = Mockery::mock(GoogleProvider::class);
+    $driver
+        ->shouldReceive('redirect')
+        ->andReturn(new RedirectResponse($redirectUrl));
+
+    Socialite::shouldReceive('driver')->andReturn($driver);
+}
+
+describe('Socialite from web routes', function () {
+    describe('fails because', function () {
+        it('has invalid provider', function () {
+            $providerName = 'whatever';
+            $user = buildSocialite(password: NULL, email: 'someone@test.com');
+
+            Provider::factory(count: 1)->create([
+                'provider' => $providerName,
+                'user_id' => $user->id
+            ]);
+            $this->get(route('oauth.redirect', ['provider' => $providerName, 'type' => 'login']));
+            $this->get(route('oauth.callback', $providerName))
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(query: [
+                        'errormsg' => Phrase::pickSentence(PhraseKey::ProviderInvalid)->toString()
+                    ])->value()
+                );
+        });
+        it('has no email parameter stored into user database table', function () {
+            buildSocialite(password: NULL, email: 'someone@test.com', createUsedDB: FALSE);
+            $this->get(route('oauth.redirect', ['provider' => 'google', 'type' => 'login']));
+            $this->get(route('oauth.callback', 'google'))
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(query: [
+                        'errormsg' => Phrase::pickSentence(PhraseKey::UserNullable)->toString()
+                    ])->value()
+                );
+        });
+        it('has email parameter with password stored into user database table', function () {
+            buildSocialite(password: fake()->password(), email: 'someone@test.com');
+            $this->get(route('oauth.redirect', ['provider' => 'google', 'type' => 'login']));
+            $this->get(route('oauth.callback', 'google'))
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(query: [
+                        'errormsg' => Phrase::pickSentence(PhraseKey::PasswordNotNullable)->toString()
+                    ])->value()
+                );
+        });
+        it('throws the ClientException', function () {
+            $route = route('oauth.callback', 'google');
+            buildSocialite(
+                password: 'whatever',
+                exception: makeClientException(uri: $route)
+            );
+            $this->get(route('oauth.redirect', ['provider' => 'google', 'type' => 'login']));
+            $this->get($route)
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(query: [
+                        'errormsg' => Phrase::pickSentence(PhraseKey::ProviderInvalid)->toString()
+                    ])->value()
+                );
+        });
+    });
+    describe('receives successful because', function () {
+        it("redirects to the correct Google's Provider sign in url", function () {
+            $redirectUrl = 'https://redirect.url';
+            configSocialiteGoogleProvider($redirectUrl);
+            $this->get(route('oauth.redirect', ['google', '1']))
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect($redirectUrl);
+        });
+        it('authenticates the "login" using Google Provider', function () {
+            buildSocialite();
+            $this->get(route('oauth.redirect', ['provider' => 'google', 'type' => 'login']));
+            $this->get(route('oauth.callback', 'google'))
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirectContains(
+                    UrlExternal::build(query: ['provided' => ''])->value()
+                );
+        });
+        it('uses the "register" using Google Provider', function () {
+            $user = buildSocialite(createUsedDB: FALSE);
+            $this->get(route('oauth.redirect', ['provider' => 'google', 'type' => 'register']));
+            $this->get(route('oauth.callback', 'google'))
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(
+                        path: config('app.frontend.uri.register.request'),
+                        query: [
+                            'provided' => $user->email
+                        ]
+                    )
+                );
+        });
+    });
+});
