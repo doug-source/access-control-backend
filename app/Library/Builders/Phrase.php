d3caaeffe40e4ee66cb89935a2595917312b9d61
Douglas Leandro <douglas.gtads@gmail.com>
2025-05-12 17:57:16 -0300

    ✨ feat: Passing uppercase from phrases as parameter

--- app/Library/Builders/Phrase.php
+++ app/Library/Builders/Phrase.php
@@ -12,7 +12,7 @@ final class Phrase
     /**
      * Determine the application's multiple sentences
      */
-    public static function pickSentence(PhraseKey $key, string $remain = '')
+    public static function pickSentence(PhraseKey $key, string $remain = '', $uppercase = TRUE)
     {
         $providers = config('services.providers');
 
@@ -20,11 +20,11 @@ final class Phrase
             PhraseKey::ParameterRequired => Str::of(__('subject-required', [
                 'subject' => __('parameter'),
                 'required' => __('required-m')
-            ]))->ucfirst(),
+            ])),
             PhraseKey::UserNullable => Str::of(__('subject-required', [
                 'subject' => __('register'),
                 'required' => __('required-m')
-            ]))->ucfirst(),
+            ])),
             PhraseKey::PasswordNotNullable => Str::of(__('subject-required', [
                 'subject' => __('login-with-password', [
                     'log-in' => __('log-in'),
@@ -32,94 +32,97 @@ final class Phrase
                     'password' => __('password'),
                 ]),
                 'required' => __('required-m')
-            ]))->ucfirst(),
+            ])),
             PhraseKey::ProviderInvalid => Str::of(__('subject-invalid', [
                 'subject' => __('provider'),
                 'invalid' => __('invalid-m')
-            ]))->ucfirst(),
+            ])),
             PhraseKey::LoginByProvider => Str::of(__('login-by-provider', [
                 'log-in' => __('log-in'),
                 'with' => __('with'),
                 'provider' => implode(' ' . _('or') . ' ', $providers),
                 'required' => __('required-m')
-            ]))->ucfirst(),
+            ])),
             PhraseKey::EmailInvalid => Str::of(__('subject-invalid', [
                 'subject' => __('email'),
                 'invalid' => __('invalid-m')
-            ]))->ucfirst(),
+            ])),
             PhraseKey::LoginInvalid => Str::of(__('subject-invalid', [
                 'subject' => __('log-in'),
                 'invalid' => __('invalid-m')
-            ]))->ucfirst(),
+            ])),
             PhraseKey::ParameterInvalid => Str::of(__('subject-invalid', [
                 'subject' => __('parameter'),
                 'invalid' => __('invalid-m')
-            ]))->ucfirst(),
+            ])),
             PhraseKey::MinSizeInvalid => Str::of(__('min-size-invalid', [
                 'minimum' => __('minimum-m'),
                 'size' => __('size'),
-            ]))->ucfirst(),
+            ])),
             PhraseKey::MaxSizeInvalid => Str::of(__('max-size-invalid', [
                 'maximum' => __('maximum'),
                 'size' => __('size'),
-            ]))->ucfirst(),
+            ])),
             PhraseKey::MinLettersInvalid => Str::of(__('min-qty-invalid', [
                 'minimum' => __('minimum-f'),
                 'quantity' => __('quantity'),
                 'subject' => __('letters')
-            ]))->ucfirst(),
+            ])),
             PhraseKey::MinUppercaseInvalid => Str::of(__('min-qty-invalid', [
                 'minimum' => __('minimum-f'),
                 'quantity' => __('quantity'),
                 'subject' => __('uppercases')
-            ]))->ucfirst(),
+            ])),
             PhraseKey::MinLowercaseInvalid => Str::of(__('min-qty-invalid', [
                 'minimum' => __('minimum-f'),
                 'quantity' => __('quantity'),
                 'subject' => __('lowercases')
-            ]))->ucfirst(),
+            ])),
             PhraseKey::MinDigitsInvalid => Str::of(__('min-qty-invalid', [
                 'minimum' => __('minimum-f'),
                 'quantity' => __('quantity'),
                 'subject' => __('digits')
-            ]))->ucfirst(),
+            ])),
             PhraseKey::MinSpecialCharsInvalid => Str::of(__('min-qty-invalid', [
                 'minimum' => __('minimum-f'),
                 'quantity' => __('quantity'),
                 'subject' => __('special-chars')
-            ]))->ucfirst(),
-            PhraseKey::RegistrationExpired => Str(__('registration-expired'))->ucfirst(),
+            ])),
+            PhraseKey::RegistrationExpired => Str(__('registration-expired')),
             PhraseKey::PassConfirmInvalid => Str::of(__('pass-confirm-invalid', [
                 'password' => __('password'),
                 'confirmation' => __('confirmation')
-            ]))->ucfirst(),
+            ])),
             PhraseKey::EmailAlreadyVerified => Str(__('verify-email-already-verified', [
                 'email' => __('email'),
                 'verified' => __('verified')
-            ]))->ucfirst(),
-            PhraseKey::Congratulations => Str::of(__('congratulations'))->ucfirst(),
-            PhraseKey::ClickHere => Str::of(__('click-here'))->ucfirst(),
-            PhraseKey::PreRegisterUserTextOne => Str::of(__('pre-register-user-text-1'))->ucfirst(),
+            ])),
+            PhraseKey::Congratulations => Str::of(__('congratulations')),
+            PhraseKey::ClickHere => Str::of(__('click-here')),
+            PhraseKey::PreRegisterUserTextOne => Str::of(__('pre-register-user-text-1')),
             PhraseKey::PreRegisterUserTextTwo => Str::of(__('pre-register-user-text-2', [
                 'button' => __('button')
-            ]))->ucfirst(),
-            PhraseKey::Regards => Str::of(__('regards'))->ucfirst(),
-            PhraseKey::Hello => Str::of(__('hello'))->ucfirst(),
+            ])),
+            PhraseKey::Regards => Str::of(__('regards')),
+            PhraseKey::Hello => Str::of(__('hello')),
             PhraseKey::ConfirmationEmail => Str::of(__('confirmation-email', [
                 'confirmation' => __('confirmation'),
                 'email' => __('email')
-            ]))->ucfirst(),
+            ])),
             PhraseKey::ConfirmationEmailText => Str::of(__('confirmation-email-text', [
                 'button' => __('button'),
                 'email-address' => __('email-address', [
                     'address' => __('address'),
                     'email' => __('email')
                 ])
-            ]))->ucfirst(),
+            ])),
             PhraseKey::RegisterApproval => Str::of(__('register-approval', [
                 'register' => __('register')
-            ]))->ucfirst(),
+            ])),
         };
+        if ($uppercase) {
+            return $message->append($remain)->ucfirst();
+        }
         return $message->append($remain);
     }
 }
