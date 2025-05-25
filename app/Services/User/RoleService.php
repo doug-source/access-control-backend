ff947e6509a6e2662aadd63be2cd61a4904f20cf
Douglas Leandro <douglas.gtads@gmail.com>
2025-05-25 19:04:54 -0300

    ✨ feat: Separating the role merging logic from role filtering logic

--- app/Services/User/RoleService.php
+++ app/Services/User/RoleService.php
@@ -19,12 +19,24 @@ final class RoleService implements RoleServiceInterface
 
     public function combine(Collection $roles, BaseCollection $namesToRemove, BaseCollection $namesToInclude): Collection
     {
+        return $this->separate(roles: $roles, namesToRemove: $namesToRemove)->concat(
+            $this->roleRepository->findByNames($namesToInclude)->all()
+        );
+    }
+
+    /**
+     * Receive role names to remove from collection role
+     *
+     * @param \Illuminate\Database\Eloquent\Collection<int, \App\Models\Role> $roles
+     * @param \Illuminate\Support\Collection<int, string> $namesToRemove
+     * @return \Illuminate\Database\Eloquent\Collection<int, \App\Models\Role>
+     */
+    private function separate(Collection $roles, BaseCollection $namesToRemove): Collection
+    {
         return $roles->reject(
             fn(Role $role) => $namesToRemove->contains(
                 fn(string $roleName) => $roleName === $role->name
             )
-        )->concat(
-            $this->roleRepository->findByNames($namesToInclude)->all()
         );
     }
 }
