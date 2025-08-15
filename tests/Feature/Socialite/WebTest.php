cb9047132df020ee398f1b9d89fe0b34099ba50f
Douglas Leandro <douglas.gtads@gmail.com>
2025-08-15 19:12:59 -0300

    🧪 test: Google provider use testing (user register request)
    Include some provider login and provider register testing adjusts

--- tests/Feature/Socialite/WebTest.php
+++ tests/Feature/Socialite/WebTest.php
@@ -5,7 +5,6 @@ use App\Library\Builders\UrlExternal;
 use App\Library\Enums\PhraseKey;
 use App\Models\Provider;
 use App\Models\RegisterPermission;
-use App\Models\User;
 use App\Repositories\UserRepository;
 use GuzzleHttp\{
     Psr7\Request,
@@ -18,6 +17,10 @@ use Laravel\Socialite\{
     Facades\Socialite,
     Two\GoogleProvider,
 };
+use App\Library\Builders\Token as TokenBuilder;
+use App\Library\Enums\ColumnSize\RegisterPermissionSize;
+use Illuminate\Support\Facades\Exceptions;
+
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
@@ -40,7 +43,26 @@ function configSocialiteGoogleProvider(string $redirectUrl)
 
 describe('Socialite from web routes', function () {
     describe('fails because', function () {
-        it('has invalid provider', function () {
+        it('has invalid type during provider request', function () {
+            Exceptions::fake();
+
+            $providerName = 'whatever';
+            $user = buildSocialite(password: NULL, email: 'someone@test.com');
+
+            Provider::factory(count: 1)->create([
+                'provider' => $providerName,
+                'user_id' => $user->id
+            ]);
+            $this->get(route('oauth.redirect', ['provider' => $providerName, 'type' => 'whatever']));
+            $this->get(route('oauth.callback', $providerName));
+
+            Exceptions::assertReported(function (\Exception $e) {
+                $msgOk = $e->getMessage() === 'Execution flow not implemented';
+                $codeOk = $e->getCode() === 1;
+                return $msgOk && $codeOk;
+            });
+        });
+        it('has invalid provider during provider login', function () {
             $providerName = 'whatever';
             $user = buildSocialite(password: NULL, email: 'someone@test.com');
 
@@ -57,8 +79,8 @@ describe('Socialite from web routes', function () {
                     ])->value()
                 );
         });
-        it('has no email parameter stored into user database table', function () {
-            buildSocialite(password: NULL, email: 'someone@test.com', createUsedDB: FALSE);
+        it('has no email parameter stored into user database table during provider login', function () {
+            buildSocialite(password: NULL, email: fake()->email(), createUsedDB: FALSE);
             $this->get(route('oauth.redirect', ['provider' => 'google', 'type' => 'login']));
             $this->get(route('oauth.callback', 'google'))
                 ->assertStatus(HttpResponse::HTTP_FOUND)
@@ -68,8 +90,8 @@ describe('Socialite from web routes', function () {
                     ])->value()
                 );
         });
-        it('has email parameter with password stored into user database table', function () {
-            buildSocialite(password: fake()->password(), email: 'someone@test.com');
+        it('has email parameter with password stored into user database table during provider login', function () {
+            buildSocialite(password: fake()->password(), email: fake()->email());
             $this->get(route('oauth.redirect', ['provider' => 'google', 'type' => 'login']));
             $this->get(route('oauth.callback', 'google'))
                 ->assertStatus(HttpResponse::HTTP_FOUND)
@@ -79,7 +101,9 @@ describe('Socialite from web routes', function () {
                     ])->value()
                 );
         });
-        it('throws the ClientException', function () {
+        it('throws the ClientException during provider login', function () {
+            Exceptions::fake();
+
             $route = route('oauth.callback', 'google');
             buildSocialite(
                 password: 'whatever',
@@ -90,7 +114,192 @@ describe('Socialite from web routes', function () {
                 ->assertStatus(HttpResponse::HTTP_FOUND)
                 ->assertRedirect(
                     UrlExternal::build(query: [
-                        'errormsg' => Phrase::pickSentence(PhraseKey::ProviderInvalid)->toString()
+                        'errormsg' => Phrase::pickSentence(PhraseKey::ProviderCredentialsInvalid)->toString()
+                    ])->value()
+                );
+        });
+        it('throws the ClientException during provider register', function () {
+            Exceptions::fake();
+
+            $route = route('oauth.callback', 'google');
+            buildSocialite(
+                password: 'whatever',
+                exception: makeClientException(uri: $route)
+            );
+            $this->get(route('oauth.redirect', [
+                'provider' => 'google',
+                'type' => 'register',
+                'token' => TokenBuilder::build()
+            ]));
+            $this->get($route)
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(query: [
+                        'errormsg' => Phrase::pickSentence(PhraseKey::ProviderCredentialsInvalid)->toString()
+                    ])->value()
+                );
+        });
+        it('has invalid email parameter during provider register', function () {
+            $route = route('oauth.callback', 'google');
+            buildSocialite(
+                password: 'whatever',
+                createUsedDB: FALSE,
+                email: 'whatever',
+            );
+            $this->get(route('oauth.redirect', [
+                'provider' => 'google',
+                'type' => 'register',
+                'token' => TokenBuilder::build()
+            ]));
+            $this->get($route)
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(query: [
+                        'errormsg' => Phrase::pickSentence(PhraseKey::EmailInvalid)->toString()
+                    ])->value()
+                );
+        });
+        it('has email parameter already stored during provider register', function () {
+            $route = route('oauth.callback', 'google');
+            buildSocialite(
+                password: 'whatever',
+                email: fake()->email(),
+            );
+            $this->get(route('oauth.redirect', [
+                'provider' => 'google',
+                'type' => 'register',
+                'token' => TokenBuilder::build()
+            ]));
+            $this->get($route)
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(query: [
+                        'errormsg' => Phrase::pickSentence(PhraseKey::EmailInvalid)->toString()
+                    ])->value()
+                );
+        });
+        it('has no token parameter during provider register', function () {
+            $route = route('oauth.callback', 'google');
+            buildSocialite(
+                password: 'whatever',
+                createUsedDB: FALSE,
+                email: fake()->email(),
+            );
+            $this->get(route('oauth.redirect', [
+                'provider' => 'google',
+                'type' => 'register',
+                'token' => ''
+            ]));
+            $this->get($route)
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(query: [
+                        'errormsg' => Phrase::pickSentence(PhraseKey::ParameterRequired)->toString()
+                    ])->value()
+                );
+        });
+        it('has token parameter overflowing the column size during provider register', function () {
+            $route = route('oauth.callback', 'google');
+            buildSocialite(
+                password: 'whatever',
+                createUsedDB: FALSE,
+                email: fake()->email(),
+            );
+            $tokenMaxSize = RegisterPermissionSize::TOKEN->get();
+            $this->get(route('oauth.redirect', [
+                'provider' => 'google',
+                'type' => 'register',
+                'token' => generateWordBySize(size: $tokenMaxSize + 1)
+            ]));
+            $this->get($route)
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(query: [
+                        'errormsg' => Phrase::pickSentence(PhraseKey::MaxSizeInvalid, " ({$tokenMaxSize})")->toString()
+                    ])->value()
+                );
+        });
+        it('has token parameter not equal to token stored during provider register', function () {
+            $route = route('oauth.callback', 'google');
+            buildSocialite(
+                password: 'whatever',
+                createUsedDB: FALSE,
+                email: fake()->email(),
+            );
+            $this->get(route('oauth.redirect', [
+                'provider' => 'google',
+                'type' => 'register',
+                'token' => TokenBuilder::build()
+            ]));
+            $this->get($route)
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(query: [
+                        'errormsg' => Phrase::pickSentence(PhraseKey::ParameterInvalid)->toString()
+                    ])->value()
+                );
+        });
+        it('has token parameter expired during provider register', function () {
+            $registerPermission = RegisterPermission::factory()->createOne([
+                'expiration_data' => now()->subtract(DateInterval::createFromDateString('2 day'))
+            ]);
+            $route = route('oauth.callback', 'google');
+            buildSocialite(
+                password: 'whatever',
+                createUsedDB: FALSE,
+                email: $registerPermission->email,
+            );
+            $this->get(route('oauth.redirect', [
+                'provider' => 'google',
+                'type' => 'register',
+                'token' => $registerPermission->token,
+            ]));
+            $this->get($route)
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(query: [
+                        'errormsg' => Phrase::pickSentence(PhraseKey::RegistrationExpired)->toString()
+                    ])->value()
+                );
+        });
+        it('throws the ClientException during provider request', function () {
+            Exceptions::fake();
+
+            $route = route('oauth.callback', 'google');
+            buildSocialite(
+                password: 'whatever',
+                exception: makeClientException(uri: $route)
+            );
+            $this->get(route('oauth.redirect', [
+                'provider' => 'google',
+                'type' => 'request',
+            ]));
+            $path = config('app.frontend.uri.register.request');
+            $this->get($route)
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(path: $path, query: [
+                        'errormsg' => Phrase::pickSentence(PhraseKey::ProviderCredentialsInvalid)->toString()
+                    ])->value()
+                );
+        });
+        it('has invalid email parameter during provider request', function () {
+            $route = route('oauth.callback', 'google');
+            buildSocialite(
+                password: 'whatever',
+                createUsedDB: FALSE,
+                email: 'whatever',
+            );
+            $this->get(route('oauth.redirect', [
+                'provider' => 'google',
+                'type' => 'request',
+            ]));
+            $path = config('app.frontend.uri.register.request');
+            $this->get($route)
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(path: $path, query: [
+                        'errormsg' => Phrase::pickSentence(PhraseKey::EmailInvalid)->toString()
                     ])->value()
                 );
         });
@@ -137,6 +346,28 @@ describe('Socialite from web routes', function () {
             $this->assertDatabaseHas('personal_access_tokens', [
                 'tokenable_id' => $user->id,
             ]);
+        });
+        it('uses the "request" using Google Provider', function () {
+            buildSocialite(email: fake()->email(), name: fake()->name(), createUsedDB: false);
+
+            $this->get(
+                route(
+                    'oauth.redirect',
+                    [
+                        'provider' => 'google',
+                        'type' => 'request',
+                    ]
+                )
+            );
+            $path = config('app.frontend.uri.register.request');
+            $this->get(route('oauth.callback', 'google'))
+                ->assertStatus(HttpResponse::HTTP_FOUND)
+                ->assertRedirect(
+                    UrlExternal::build(
+                        path: $path,
+                        query: ['successmsg' => Phrase::pickSentence(key: PhraseKey::Registered, remain: '!')->toString()]
+                    )->value()
+                );
         });
     });
 });
